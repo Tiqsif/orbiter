9448d7fe6af6d94e49db195c0d48d627a5404dac
Tiqsif <yekurtarici@gmail.com>
2024-11-07 18:10:32 +0300

    indicator update
    two mesh for indicator. transparent one from center to ending point. opaque one from center to current point

--- Assets/Scripts/Orb.cs
+++ Assets/Scripts/Orb.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Orb : MonoBehaviour
 {
     public float width = 1;
-    public Material indicatorMaterial;
+    public Material indicatorMaterial0;
+    public Material indicatorMaterial1;
     protected Vector3 direction; // direction of the orb, has the speed in it
     protected bool hasIndicator = false;
     protected AttackIndicator indicator;
@@ -29,7 +30,8 @@ public class Orb : MonoBehaviour
         // create a mesh from center to the ending point on the circle with low alpha color
         GameObject indicatorObj = new GameObject("Indicator");
         indicator = indicatorObj.AddComponent<AttackIndicator>();
-        indicator.material = indicatorMaterial;
+        indicator.material0 = indicatorMaterial0;
+        indicator.material1 = indicatorMaterial1;
         indicator.Create(arcManager, direction, width);
         // create a mesh from center to the orb position with high alpha color
     }
